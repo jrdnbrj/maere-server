7bae5616c17b97a93c2b8f7ac7aab690338c10ab
Jordan Borja <www.jrdnbrj@gmail.com>
2021-10-13 23:38:54 -0500

    Project Basis

---
+++ main.py
@@ -0,0 +1,31 @@
+# GraphQL
+from starlette.graphql import GraphQLApp
+
+# Schema Definition
+from src.schema import schema
+
+# FastAPI
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+# .env
+from decouple import config
+
+# mongoengine
+from mongoengine import connect
+
+
+app = FastAPI()
+
+app.add_route("/gql", GraphQLApp(schema=schema))
+
+connect(host=config('MONGO_SRV'))
+
+# https://fastapi.tiangolo.com/tutorial/cors/
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
